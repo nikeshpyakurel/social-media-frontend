322494c3fb5489d336e8d2512eb207d29b2b876c
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-01-18 16:19:06 +0545

    assets

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import { useContext } from "react";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { AuthContext } from "./context/AuthContext";
 import {
-  BrowserRouter, Routes, Route, Navigate
+  BrowserRouter, Routes, Route, Navigate,
 } from "react-router-dom"
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </BrowserRouter>
