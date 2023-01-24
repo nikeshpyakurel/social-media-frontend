2935784140fd63141a5148c59b607c96af351734
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-01-24 15:42:47 +0545

    Messenger Router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { AuthContext } from "./context/AuthContext";
 import {
   BrowserRouter, Routes, Route, Navigate,
 } from "react-router-dom"
+import Messenger from "./pages/messenger/Messenger";
 function App() {
   const { user } = useContext(AuthContext);
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={user ? <Home /> : <Register />} />
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
         <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+        <Route path="/messenger" element={!user ? <Navigate to="/" /> : <Messenger />} />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </BrowserRouter>
