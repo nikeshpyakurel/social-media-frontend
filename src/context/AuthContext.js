815b0e23699b2c0988885ed70f88cb25e0796404
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-01-24 15:42:27 +0545

    Save user data to local storage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,21 +1,10 @@
-import { createContext, useReducer } from "react"
+import { createContext, useReducer, useEffect } from "react"
 import AuthReducer from "./AuthReducer";
 const INITIAL_STATE = {
-    user: {
-        _id: "63be4288b5e6e22cc7bdb990",
-        username: "prayash",
-        email: "prayash@gmail.com",
-        password: "$2b$10$4n5y5AcRv6ywjbbBH0ie/e1Q.4cXw27OXleYnmtk6X/ZBE1kq.Gwi",
-        profilePicture: "",
-        coverPicture: "",
-        followers: [],
-        following: [
-            "63be4278b5e6e22cc7bdb98e"
-        ],
-    },
+    user: JSON.parse(localStorage.getItem("user")) || null,
     isFetching: false,
     error: false,
-}
+};
 
 
 export const AuthContext = createContext(INITIAL_STATE);
@@ -23,6 +12,9 @@ export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(state.user))
+    }, [state.user])
     return (
         <AuthContext.Provider
             value={{ user: state.user, isFetching: state.isFetching, error: state.error, dispatch }}
